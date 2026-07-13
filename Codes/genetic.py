371c8270743c8ed9a440cccb88b4dd2ad30fe429
levia <rifatabdullah754@gmail.com>
2026-07-13 22:28:23 +0600

    kkj

--- Codes/genetic.py
+++ Codes/genetic.py
@@ -5,5 +5,13 @@ def random_path(no_of_destination):
     random_paths = []
     
     for i in range(5):
-        random_paths = list(range(1, no_of_destination))
+        random_pathx = list(range(0, no_of_destination))
+        
+        random.shuffle(random_pathx)
+        #random_pathx =  random_pathx
+        random_paths.append(random_pathx)
+    return random_paths
 
+
+x = random_path(5)
+print(x)
